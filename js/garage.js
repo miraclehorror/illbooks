2b60b307682f45ce99c4b1a3005de80428d4c4c3
ooj2434 <positron.electron@yahoo.com>
2018-05-25 16:19:28 +0200

    second commit

--- js/garage.js
+++ js/garage.js
@@ -1,54 +1,14 @@
-/**
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var numbering = document.getElementsByClassName("slide-number-down");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < numbering.length; i++) {
-      numbering[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";  
-  numbering[slideIndex-1].style.display = "inline";
-  dots[slideIndex-1].className += " active";
-} 
-
-
-
-function showExplanation() {
-  var i;
-  var explanations = document.getElementsByClassName("explanation");
-  for (i = 0; i < explanations.length; i++) {
-	  explanations[i].style.display = "none";	
-  }
-}
-
-*/
 
 function showExplanation(artefact) {
+  var i;
+  var explanations = document.getElementsByClassName("explanation");	
   var explanation = document.getElementById(artefact);
+  
+  for (i=0; i < explanations.length; i++) {
+	  explanations[i].style.display = "none";
+  }
+  
   explanation.style.display = "block";	
 }
 
--- js/main.js
+++ js/main.js
@@ -32,32 +32,20 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var numbering = document.getElementsByClassName("slide-number-down");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < numbering.length; i++) {
-      numbering[i].style.display = "none";
-  }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
 
   slides[slideIndex-1].style.display = "block";  
-  numbering[slideIndex-1].style.display = "inline";
   dots[slideIndex-1].className += " active";
 } 
 
-function showExplanation() {
-  var i;
-  var explanations = document.getElementsByClassName("explanation");
-  for (i = 0; i < explanations.length; i++) {
-	  explanations[i].style.display = "none";	
-  }
-}
 
 
 
